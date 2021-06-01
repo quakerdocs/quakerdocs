a8e5e0a67b7f51b8ba2dbc3ba0c4fbbc98022841
Bob Reus <bobreus950@gmail.com>
2021-06-01 16:59:09 +0200

    script.js cleanup

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,23 +1,16 @@
 
-function inject(filename, element) {
+function inject(filename, src_element, dst_element) {
     fetch(filename, {method: 'GET', mode: 'no-cors', credentials: 'same-origin'})
-        .then(response => {
-            console.log(response)
-            console.log(response.ok)
-            return response.text()
-        })
+        .then(res => res.text())
         .then(data => {
-            console.log(data)
-            var parser = new DOMParser();
-            var htmlDoc = parser.parseFromString(data, 'text/html');
-            content = htmlDoc.getElementsByClassName('section')[0].innerHTML
-            console.log(content)
-            document.querySelector(element).innerHTML = content
+            var parser = new DOMParser()
+            var htmlDoc = parser.parseFromString(data, 'text/html')
+            content = htmlDoc.querySelector(src_element).innerHTML
+            document.querySelector(dst_element).innerHTML = content
         })
         .catch(console.log)
 }
 
 window.onload = function(e) {
-    console.log("pep")
-    inject('./pages/checking-for-plagiarism.html', '#content')
+    inject('./pages/checking-for-plagiarism.html', 'div.section', 'div#content')
 }
