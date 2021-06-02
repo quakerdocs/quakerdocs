2c85039e29a443cc9d8e9131d3193b8d95facc53
Philip Schutte <ps.schutte@ziggo.nl>
2021-06-02 15:58:52 +0200

    Placeholder search results show up in searchbox

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -26,8 +26,83 @@ function toggleFunction() {
 
 function overlayOn() {
     document.getElementById("overlay").style.display = "block";
+    activateSearch();
 }
 
 function overlayOff() {
     document.getElementById("overlay").style.display = "none";
+}
+
+class Result {
+    constructor(page, title, paragraph="") {
+        // The page URL.
+        this.page = page;
+
+        // The title of the page, for quick display.
+        this.title = title;
+
+        // Optionally the paragraph title, to allow searching on paragraph level.
+        // This is a stretch goal, so it doesn't have to be supported right away.
+        this.paragraph = paragraph;
+    }
+};
+
+function activateSearch() {
+    const searchInput = document.getElementById("searchbar");
+    const resultsWrapper = document.getElementById("search-results")
+
+    if (searchInput) {
+        searchInput.addEventListener('keyup', () => {
+            let input = searchInput.value;
+            if (input.length) {
+                let searcher = performSearch(input);
+                renderResults(searcher, resultsWrapper);
+            } else {
+                resultsWrapper.innerHTML = '';
+            }
+        })
+    }
+}
+
+function* performSearch(query) {
+    // Nothing is done with query as of now.
+
+    let n_pages = Math.floor(Math.random() * 5) + 1;
+
+    random_pages = [
+        new Result("index.html", "Index"),
+        new Result("about/about.html", "About"),
+        new Result("guides/autotest-best-practices.html", "autotest best practices"),
+        new Result("guides/cannot-find-feature-in-codegrade.html", "cannot find feature in codegrade"),
+        new Result("guides/checking-for-plagiarism.html", "checking for plagiarism"),
+        new Result("guides/creating-an-assignment.html", "creating an assignment"),
+        new Result("guides/creating-managing-and-using-snippets.html", "creating managing and using snippets"),
+        new Result("guides/dividing-submissions.html", "dividing submissions"),
+        new Result("guides/enabling-continuous-feedback-for-an-assignment.html", "enabling continuous feedback for an assignment"),
+        new Result("guides/getting-started-with-codegrade.html", "getting started with codegrade"),
+        new Result("guides/installing-codegrade-filesystem.html", "installing codegrade filesystem"),
+        new Result("guides/pass-back-grades-to-canvas-blackboard-moodle-brightspace.html", "pass back grades to canvas blackboard moodle brightspace"),
+        new Result("guides/setting-up-autotest.html", "setting up autotest"),
+        new Result("guides/setting-up-git-uploads.html", "setting up git uploads"),
+        new Result("guides/setting-up-hand-in-requirements.html", "setting up hand in requirements"),
+        new Result("guides/set-up-a-rubric-for-an-assignment.html", "set up a rubric for an assignment"),
+        new Result("guides/set-up-group-assignment.html", "set up group assignment"),
+        new Result("guides/setup-password-for-codegrade-account.html", "setup password for codegrade account"),
+        new Result("guides/use-codegrade-as-a-student.html", "use codegrade as a student"),
+        new Result("guides/use-codegrade-filesystem.html", "use codegrade filesystem"),
+        new Result("guides/using-shortcuts.html", "using shortcuts")
+    ];
+
+    for (i = 0; i < n_pages; i++) {
+        yield random_pages[Math.floor(Math.random() * random_pages.length)];
+    }
+}
+
+function renderResults(searcher, resultsWrapper) {
+    let content = '';
+    for (let r of searcher) {
+        content += `<li><a href=${r.page}>${r.title}</a></li>`
+    }
+
+    resultsWrapper.innerHTML = `<ul>${content}</ul>`;
 }
