e471aaf691a17b638201c9853e02d199757ece06
Bob Reus <bobreus950@gmail.com>
2021-06-03 17:17:18 +0200

    updated search scripts

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -35,20 +35,20 @@ function hideSearchOverlay() {
     document.body.scroll = "yes";
 }
 
-class Result {
-    constructor(page, title, paragraph="") {
-        // The page URL.
-        this.page = page;
+// class Result {
+//     constructor(page, title, paragraph="") {
+//         // The page URL.
+//         this.page = page;
 
-        // The title of the page, for quick display.
-        this.title = title;
+//         // The title of the page, for quick display.
+//         this.title = title;
 
-        // Optionally the paragraph title, to allow searching on paragraph
-        // level. This is a stretch goal, so it doesn't have to be supported
-        // right away.
-        this.paragraph = paragraph;
-    }
-};
+//         // Optionally the paragraph title, to allow searching on paragraph
+//         // level. This is a stretch goal, so it doesn't have to be supported
+//         // right away.
+//         this.paragraph = paragraph;
+//     }
+// };
 
 function activateSearch() {
     /* Activate the search bar with an event listener.
@@ -70,39 +70,39 @@ function activateSearch() {
     }
 }
 
-function* performSearch(query) {
-    // Nothing is done with query as of now.
+// function* performSearch(query) {
+//     // Nothing is done with query as of now.
 
-    let n_pages = Math.floor(Math.random() * 5) + 1;
+//     let n_pages = Math.floor(Math.random() * 5) + 1;
 
-    random_pages = [
-        new Result("index.html", "Index"),
-        new Result("about/about.html", "About"),
-        new Result("guides/autotest-best-practices.html", "autotest best practices"),
-        new Result("guides/cannot-find-feature-in-codegrade.html", "cannot find feature in codegrade"),
-        new Result("guides/checking-for-plagiarism.html", "checking for plagiarism"),
-        new Result("guides/creating-an-assignment.html", "creating an assignment"),
-        new Result("guides/creating-managing-and-using-snippets.html", "creating managing and using snippets"),
-        new Result("guides/dividing-submissions.html", "dividing submissions"),
-        new Result("guides/enabling-continuous-feedback-for-an-assignment.html", "enabling continuous feedback for an assignment"),
-        new Result("guides/getting-started-with-codegrade.html", "getting started with codegrade"),
-        new Result("guides/installing-codegrade-filesystem.html", "installing codegrade filesystem"),
-        new Result("guides/pass-back-grades-to-canvas-blackboard-moodle-brightspace.html", "pass back grades to canvas blackboard moodle brightspace"),
-        new Result("guides/setting-up-autotest.html", "setting up autotest"),
-        new Result("guides/setting-up-git-uploads.html", "setting up git uploads"),
-        new Result("guides/setting-up-hand-in-requirements.html", "setting up hand in requirements"),
-        new Result("guides/set-up-a-rubric-for-an-assignment.html", "set up a rubric for an assignment"),
-        new Result("guides/set-up-group-assignment.html", "set up group assignment"),
-        new Result("guides/setup-password-for-codegrade-account.html", "setup password for codegrade account"),
-        new Result("guides/use-codegrade-as-a-student.html", "use codegrade as a student"),
-        new Result("guides/use-codegrade-filesystem.html", "use codegrade filesystem"),
-        new Result("guides/using-shortcuts.html", "using shortcuts")
-    ];
+//     random_pages = [
+//         new Result("index.html", "Index"),
+//         new Result("about/about.html", "About"),
+//         new Result("guides/autotest-best-practices.html", "autotest best practices"),
+//         new Result("guides/cannot-find-feature-in-codegrade.html", "cannot find feature in codegrade"),
+//         new Result("guides/checking-for-plagiarism.html", "checking for plagiarism"),
+//         new Result("guides/creating-an-assignment.html", "creating an assignment"),
+//         new Result("guides/creating-managing-and-using-snippets.html", "creating managing and using snippets"),
+//         new Result("guides/dividing-submissions.html", "dividing submissions"),
+//         new Result("guides/enabling-continuous-feedback-for-an-assignment.html", "enabling continuous feedback for an assignment"),
+//         new Result("guides/getting-started-with-codegrade.html", "getting started with codegrade"),
+//         new Result("guides/installing-codegrade-filesystem.html", "installing codegrade filesystem"),
+//         new Result("guides/pass-back-grades-to-canvas-blackboard-moodle-brightspace.html", "pass back grades to canvas blackboard moodle brightspace"),
+//         new Result("guides/setting-up-autotest.html", "setting up autotest"),
+//         new Result("guides/setting-up-git-uploads.html", "setting up git uploads"),
+//         new Result("guides/setting-up-hand-in-requirements.html", "setting up hand in requirements"),
+//         new Result("guides/set-up-a-rubric-for-an-assignment.html", "set up a rubric for an assignment"),
+//         new Result("guides/set-up-group-assignment.html", "set up group assignment"),
+//         new Result("guides/setup-password-for-codegrade-account.html", "setup password for codegrade account"),
+//         new Result("guides/use-codegrade-as-a-student.html", "use codegrade as a student"),
+//         new Result("guides/use-codegrade-filesystem.html", "use codegrade filesystem"),
+//         new Result("guides/using-shortcuts.html", "using shortcuts")
+//     ];
 
-    for (i = 0; i < n_pages; i++) {
-        yield random_pages[Math.floor(Math.random() * random_pages.length)];
-    }
-}
+//     for (i = 0; i < n_pages; i++) {
+//         yield random_pages[Math.floor(Math.random() * random_pages.length)];
+//     }
+// }
 
 function renderResults(searcher, resultsWrapper) {
     /* Render the title of the search results aswell as a little bit
