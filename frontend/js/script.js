8725eba39dddc708180e5edc6e0bf71c5b3ab3b4
Bob Reus <bobreus950@gmail.com>
2021-06-03 22:27:39 +0200

    search overlay tweaks

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,4 @@
 
-const ROOT = document.location.hostname
-
 function toggleFunction() {
     /* Used to toggle the menu on small screens when clicking on the menu
      * button.
@@ -17,6 +15,9 @@ function showSearchOverlay() {
     /* Active the search bar overlay.
      */
     document.getElementById("search").classList.add("is-active")
+    searchbar = document.getElementById("searchbar")
+    searchbar.focus()
+    searchbar.select()
 
     // Prevent background from scrolling while search window is open.
     document.documentElement.style.overflow = 'hidden';
@@ -34,21 +35,6 @@ function hideSearchOverlay() {
     document.documentElement.style.overflow = 'scroll';
     document.body.scroll = "yes";
 }
-
-// class Result {
-//     constructor(page, title, paragraph="") {
-//         // The page URL.
-//         this.page = page;
-
-//         // The title of the page, for quick display.
-//         this.title = title;
-
-//         // Optionally the paragraph title, to allow searching on paragraph
-//         // level. This is a stretch goal, so it doesn't have to be supported
-//         // right away.
-//         this.paragraph = paragraph;
-//     }
-// };
 
 function activateSearch() {
     /* Activate the search bar with an event listener.
@@ -70,40 +56,6 @@ function activateSearch() {
     }
 }
 
-// function* performSearch(query) {
-//     // Nothing is done with query as of now.
-
-//     let n_pages = Math.floor(Math.random() * 5) + 1;
-
-//     random_pages = [
-//         new Result("index.html", "Index"),
-//         new Result("about/about.html", "About"),
-//         new Result("guides/autotest-best-practices.html", "autotest best practices"),
-//         new Result("guides/cannot-find-feature-in-codegrade.html", "cannot find feature in codegrade"),
-//         new Result("guides/checking-for-plagiarism.html", "checking for plagiarism"),
-//         new Result("guides/creating-an-assignment.html", "creating an assignment"),
-//         new Result("guides/creating-managing-and-using-snippets.html", "creating managing and using snippets"),
-//         new Result("guides/dividing-submissions.html", "dividing submissions"),
-//         new Result("guides/enabling-continuous-feedback-for-an-assignment.html", "enabling continuous feedback for an assignment"),
-//         new Result("guides/getting-started-with-codegrade.html", "getting started with codegrade"),
-//         new Result("guides/installing-codegrade-filesystem.html", "installing codegrade filesystem"),
-//         new Result("guides/pass-back-grades-to-canvas-blackboard-moodle-brightspace.html", "pass back grades to canvas blackboard moodle brightspace"),
-//         new Result("guides/setting-up-autotest.html", "setting up autotest"),
-//         new Result("guides/setting-up-git-uploads.html", "setting up git uploads"),
-//         new Result("guides/setting-up-hand-in-requirements.html", "setting up hand in requirements"),
-//         new Result("guides/set-up-a-rubric-for-an-assignment.html", "set up a rubric for an assignment"),
-//         new Result("guides/set-up-group-assignment.html", "set up group assignment"),
-//         new Result("guides/setup-password-for-codegrade-account.html", "setup password for codegrade account"),
-//         new Result("guides/use-codegrade-as-a-student.html", "use codegrade as a student"),
-//         new Result("guides/use-codegrade-filesystem.html", "use codegrade filesystem"),
-//         new Result("guides/using-shortcuts.html", "using shortcuts")
-//     ];
-
-//     for (i = 0; i < n_pages; i++) {
-//         yield random_pages[Math.floor(Math.random() * random_pages.length)];
-//     }
-// }
-
 function renderResults(searcher, resultsWrapper) {
     /* Render the title of the search results aswell as a little bit
      * (200 characters) of text contained in the article.
@@ -112,14 +64,12 @@ function renderResults(searcher, resultsWrapper) {
     resultsWrapper.innerHTML = '<ul id="result-list"></ul>';
 
     for (let r of searcher) {
-        let text = "";
+        id = r.title.replace(/ /g, "-");
+        resultEl = `<div id="${id}"><a class="panel-block result is-flex-direction-column" href="../${r.page}">
+                    <h1 class="result-title"><strong>${r.title}</strong></h1>
+                    <p class="result-content"></p></a></div>`
 
-        id = r.title.replaceAll(' ', '-');
-        $('#result-list').append(`<div onclick="location.href='../${r.page}'"
-                                 class="result" id="${id}"><h1 class=
-                                 "result-title"><strong>${r.title}</strong>
-                                 </h1><p class="result-content"></p></div>`);
-
+        $('#result-list').append(resultEl);
 
         // Send ajax request to get text from the page.
         $.ajax({
@@ -137,9 +87,8 @@ function renderResults(searcher, resultsWrapper) {
                     return;
                 }
 
-                id = this.title.replaceAll(' ', '-')
-                $("#" + id + " .result-content").text($.trim(p_text)
-                                                .substr(0, 200) + "...");
+                id = this.title.replace(/ /g, "-");
+                $("#" + id + " .result-content").text($.trim(p_text).substr(0, 200) + "...");
             }
         });
     }
