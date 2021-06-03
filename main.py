a5957c69e58bcf690aad132a6efeede9140a7d01
Timo Sluis <timo.sluis@live.nl>
2021-06-03 10:17:45 +0200

    Added first few Sphinx directives for RST

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import argparse
 import os
 
 import docutils.core
-import docutils.writers.html5_polyglot
-from docutils.parsers.rst import Directive
-from docutils.parsers.rst import directives
-from docutils.parsers.rst.directives.misc import Class
-from docutils.parsers.rst.directives.misc import Include
+
+import html5writer
+import custom_dirs
+import spdirs
 
 
 # https://stackoverflow.com/questions/38834378/path-to-a-directory-as-argparse-argument
@@ -23,21 +22,30 @@ def dir_path(string):
 
 
 def main():
+    """
+    Main entrypoint of the program.
+    """
     arg_parser = argparse.ArgumentParser(description='SDG')
     arg_parser.add_argument('source_path', type=dir_path, help='The directory containing the RST files.')
     arg_parser.add_argument('destination_path', type=str, help='The directory to write the output.')
     arg_parser.add_argument('-b', type=str, dest='builder', default="html", help='Builder used for the generator.')
     args = arg_parser.parse_args()
 
-    print("Hello, World!")
-    print(args.source_path, args.destination_path, args.builder)
+    print("Running SDG 0.0.1")
 
-    directives.register_directive('rst-class', Class)
-    directives.register_directive('include', Include)
-
+    # Check if destination path exists, otherwise create it.
     if not os.path.exists(args.destination_path):
+        print("Making output directory...")
         os.mkdir(args.destination_path)
     args.destination_path = dir_path(args.destination_path)
+
+    # Set-up reStructuredText directives
+    spdirs.setup()
+    custom_dirs.setup()
+
+    # Load user configuration
+    # Check if file exists? Other cwd?
+    exec(open(os.path.join(args.source_path, 'conf.py')).read())
 
     slen = len(args.source_path)
     for root, dirs, files in os.walk(args.source_path):
@@ -50,8 +58,18 @@ def main():
                 print("==================== %s ====================" % file)
                 src = os.path.join(root, file)
                 dest = os.path.join(args.destination_path, root[slen+1:], file[:-4] + '.html')
-                docutils.core.publish_file(source_path=src, destination_path=dest, writer_name=args.builder)
+                with open(src) as f:
+                    document = docutils.core.publish_doctree(source=f.read(), source_path=src)
+                    write_file = open(dest, 'wb')
+                    write_file.write(docutils.core.publish_from_doctree(document, destination_path=dest, writer=html5writer.Writer()))
+                    write_file.close()
+
+    print("The generated documents have been saved in %s" % args.destination_path)
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    """
+    Redirect to main-function
+    """
+    exit(main())
