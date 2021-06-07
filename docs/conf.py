4163f0af3a81472b9e59b07cb6c63f409f2eb964
Timo Sluis <timo.sluis@live.nl>
2021-06-07 13:12:39 +0200

    Removed test extension from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,6 @@ author = 'CodeGrade'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'test'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
