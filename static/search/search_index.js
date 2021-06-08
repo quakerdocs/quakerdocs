7bbbda7aec08a3dc1209ca3400965fd937eadfe5
Steven Lageveen <stevenmax888@gmail.com>
2021-06-08 14:29:17 +0200

    Added comments and modified search to return an object.

--- static/search/search_index.js
+++ static/search/search_index.js
@@ -1,13 +1,34 @@
+class Result {
+    constructor(page, title, paragraph="") {
+        /* The page URL. */
+        this.page = page;
 
+        /* The title of the page, for quick display. */
+        this.title = title;
+
+        /* Optionally the paragraph title, to allow searching on paragraph
+        level. This is a stretch goal, so it doesn't have to be supported
+        right away. */
+        this.paragraph = paragraph;
+    }
+};
+
+
+/* Function to use the wasm module to perform the search and return any
+   results. */
 function* performSearch(query) {
-    // TODO: check if the wasm has fully loaded.
 
+    /* Get the functions from the wasm module. */
     var search = Module.cwrap('performSearch', null, ['string']);
     var getres = Module.cwrap('getSearch', 'string');
+
+    /* Perform the search. */
     search(query);
 
+    /* Return the results as they are asked. */
     while (result = getres()) {
-        // TODO: translate to Result object. (split it on \n)
-        yield result;
+        /* Split on new line, regex to work on all os'es. */
+        let sep = result.split(/\r\n|\n\r|\n|\r/);
+        yield Result(sep[0], se[[1]]);
     }
 }
