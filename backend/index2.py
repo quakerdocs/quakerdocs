ac243f5782f7ff673c14b51e9812402fccedf07b
Steven Lageveen <stevenmax888@gmail.com>
2021-06-09 22:35:21 +0200

    Yay comments

--- backend/index2.py
+++ backend/index2.py
@@ -1,9 +1,9 @@
+# TODO maybe change page, count to data for easier expansion.
+
 import re
 import nltk
 import json
 import struct
-import string
-import doctest
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
@@ -16,7 +16,24 @@ except LookupError:
 
 
 class Trie:
+    """
+    A class to represent a Radix tree/trie
+
+    Attributes:
+        char (string): The string connecting the node to its parent.
+        end  (bool): Signifies wether node is an endpoint.
+        children (dict): A dictionary connecting chars to child nodes.
+        page_count ([(int, int)]): A list of page indexes and word counts.
+    """
+
     def __init__(self, char):
+        """
+        Constructor for the Trie class.
+
+        Parameters:
+            char (string): The string connecting the node to its parent.
+        """
+
         # Current character.
         self.char = char
 
@@ -29,14 +46,22 @@ class Trie:
         # The pages where this word is found.
         self.page_count = []
 
-    def scan_children(self, word, current):
+    def insert_helper(self, word, current):
+        """
+        A helper method used to insert a fragment of a word into the trie.
+
+        Parameters:
+            word (string): The remainder of the word to insert a fragment of.
+            current (Trie): The node of the trie from which to start searching.
+        """
+
         # Loop over the children of the current node.
         for c_word, child in current.children.items():
             # Find how well the current node matches the word.
-            match, c_rem, word = self.match(c_word, word)
+            match, rem, word = self.match(c_word, word)
 
             # Exact match to start of word.
-            if not c_rem:
+            if not rem:
                 # Move a layer deeper.
                 return word, child
 
@@ -47,16 +72,15 @@ class Trie:
                 current.children[match] = new
 
                 # Update the char of the child.
-                child.char = c_rem
+                child.char = rem
 
                 # Move the current child to the new node.
-                new.children = {c_rem: child}
+                new.children = {rem: child}
                 del current.children[c_word]
 
                 # Continue after the partial match.
                 current = new
                 break
-
 
         # If none of the children matched add the remainder as a child.
         new = Trie(word)
@@ -64,45 +88,46 @@ class Trie:
         return "", new
 
     def insert(self, word, page, count):
+        """
+        Insert method to insert a new word into the trie.
+
+        Parameters:
+            word (string): The word to be inserted.
+            page (int): The index of the page the word is in.
+            count (int): The number of times the word is found.
+        """
         if not word:
             return
 
         # Start at the root.
         current = self
 
-        # https://leetcode.com/problems/implement-trie-prefix-tree/discuss/653851/python-radix-tree-memory-efficient
-
         # Continue until the end of the word.
         while word:
-            word, current = self.scan_children(word, current)
+            word, current = self.insert_helper(word, current)
 
         # If while loop exited it means that the current node is an end node.
         current.end = True
         current.page_count.append((page, count))
-        return
 
     @staticmethod
     def match(n_word, s_word):
+        """
+        Method to match a part of a word to a part in the node.
+
+        Parameters:
+            n_word (string): The word fragment inside the node.
+            s_word (string): The fragment of the word being searched.
+        """
+
+        # Loop over both words.
         for i, (n_char, s_char) in enumerate(zip(n_word, s_word)):
+            # Stop on first difference.
             if n_char != s_char:
                 break
+
+        # Return matching part and both remainders.
         return n_word[:i], n_word[i:], s_word[i:]
-
-    # def optimise(self):
-    #     """Turn this Trie into a Radix Trie"""
-    #     stack = [self]
-    #     while stack:
-    #         node = stack.pop()
-
-    #         while len(node.children) == 1:
-    #             child = node.children[0]
-
-    #             node.children = child.children
-    #             node.char += child.char
-    #             node.end = child.end
-    #             node.page_count = child.page_count
-
-    #         stack.extend(node.children.values())
 
     def to_binary(self):
         """Translate the Trie to a binary format"""
@@ -118,7 +143,8 @@ class Trie:
 
             # Sort the children alphabetically based on the key.
             node.sorted_children = []
-            for key, child in sorted(node.children.items(), key=lambda c: c[0]):
+            for key, child in sorted(node.children.items(),
+                                     key=lambda c: c[0]):
                 child.key = key
                 node.sorted_children.append(child)
 
@@ -152,7 +178,6 @@ class Trie:
         }
 
         # Write the nodes to binary
-
         #
         # struct Node {
         #     int chars;       // (pointer)
@@ -166,12 +191,19 @@ class Trie:
 
 
 class IndexGenerator:
-    def __init__(self):
-        self.urltitles = []  # [(url, title), ...]
-        self.trie = Trie("") # root of the prefix trie
-        self.stemmer = SnowballStemmer(language="english").stem
+    """
+    A class to generate the indexing/trie used for searching as well as the
+    translation of page indices to page info.
+    """
 
-        # keep_chars = string.ascii_lowercase + string.digits + '\\s'
+    def __init__(self):
+        """
+        Constructor for the IndexGenerator class.
+        """
+        self.urltitles = []   # [(url, title), ...]
+        self.trie = Trie("")  # root of the prefix trie
+
+        self.stemmer = SnowballStemmer(language="english").stem
         self.remover = re.compile('[^\\w\\s]')
 
     def parse_file(self, content, title, url):
@@ -182,20 +214,25 @@ class IndexGenerator:
         :param str title: The title of the document
         :param str url: The url of the page
         """
+
+        # Change to lowercase, separate _ and only keep letters/numbers.
         content = content.lower().replace('_', '')
         content = self.remover.sub('', content)
 
-        keep_chars = string.ascii_lowercase + string.digits
+        # Remove stopwords.
         content = [self.stemmer(word)
                    for word in content.split() if word not in stopwords]
+
+        # Count occurences of words in page.
         word_counter = Counter(content)
 
+        # Get the index of the current page and update the list.
         i = len(self.urltitles)
         self.urltitles.append((url, title))
 
+        # Create the trie.
         for word, count in sorted(word_counter.items(), key=lambda x: x[1]):
             self.trie.insert(word, i, count)
-
 
     def to_json(self):
         """
@@ -207,7 +244,3 @@ class IndexGenerator:
 
     def to_binary(self):
         return self.trie.to_binary()
-
-# if __name__ == "__main__":
-    # doctest.testmod(verbose=True)
-    # test()
