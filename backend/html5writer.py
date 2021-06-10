cf5a852aa0db7b637bc71e5b1014202fbbb2c52c
Lars Reddering <larsreddering@outlook.com>
2021-06-10 13:30:30 +0200

    Added requirements bs4 and pygment, fixed flake8

--- backend/html5writer.py
+++ backend/html5writer.py
@@ -161,7 +161,7 @@ class HTMLTranslator(docutils.writers.html5_polyglot.HTMLTranslator):
         id = self.create_bookmark_id(node)
         onclick = f"bookmarkClick('{id}')"
         bookmark_html = f'<button id="{id}" class="bookmark-btn" onclick="{onclick}" title="{title}" value=0>' + \
-                         '<span class="icon"><i class="fa fa-bookmark-o"></i></span></button>'
+                        '<span class="icon"><i class="fa fa-bookmark-o"></i></span></button>'
         self.body.append(bookmark_html)
 
     # ! Needs to be improved !
