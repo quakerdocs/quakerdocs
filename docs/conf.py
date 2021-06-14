bdb5f7a755d01ec85fe8e9f36bace14d188f8818
pira99 <piraveen@live.nl>
2021-06-14 15:49:34 +0200

    The docs for our QuakerDocs

--- docs/conf.py
+++ docs/conf.py
@@ -92,4 +92,7 @@ html_theme = 'quakerdocs'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+
+html_favicon = '_static/_images/favicon.ico'
+html_logo = '_static/_images/logo.png'
