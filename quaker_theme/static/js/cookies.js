ea385226eb6292d7739365f84e707ead0e33214f
Bob Reus <bobreus950@gmail.com>
2021-06-16 12:44:03 +0200

    improve cookies.js code style

--- quaker_theme/static/js/cookies.js
+++ quaker_theme/static/js/cookies.js
@@ -4,22 +4,21 @@
  * @param {string} cname The name of the new cookie.
  * @param {*} cvalue The value of the cookie.
  */
-function setCookie(cname, cvalue) {
-    let path = "/";
-    let date = new Date();
-    date.setFullYear(2999);
+function setCookie (cname, cvalue) {
+    const path = '/'
+    const date = new Date()
+    date.setFullYear(2999)
 
-    document.cookie = `${cname}=${cvalue};expires=${date};path=${path};SameSite=Lax;Secure`;
+    document.cookie = `${cname}=${cvalue};expires=${date};path=${path};SameSite=Lax;Secure`
 }
 
 /**
  * Retrieve all cookies from the website.
  * @returns {Array} Array of all cookies.
  */
-function getCookieList() {
-    let cookies = decodeURIComponent(document.cookie);
-    let clist = cookies.split(';');
-    return clist;
+function getCookieList () {
+    const cookies = decodeURIComponent(document.cookie)
+    return cookies.split(';')
 }
 
 /**
@@ -27,26 +26,26 @@ function getCookieList() {
  * @param {string} cname The name of the to be retrieved cookie.
  * @returns {*} The value of the cookie or null if the cookie wasn't found.
  */
-function getCookie(cname) {
-    let clist = getCookieList();
-    for (let i = 0; i < clist.length; i++) {
-        let name, val;
-        [name, val] = clist[i].split('=');
+function getCookie (name) {
+    const cList = getCookieList()
 
-        if (name.trim() === cname.trim()) {
-            return val;
+    for (const cookie of cList) {
+        const [name2, val] = cookie.split('=')
+
+        if (name.trim() === name2.trim()) {
+            return val
         }
     }
 
-    return null;
+    return null
 }
 
 /**
  * Delete the cookie with name=cname.
  * @param {string} cname The name of the to be deleted cookie.
  */
-function deleteCookie(cname) {
-    if (getCookie(cname) != null) {
-        setCookie(cname, "");
+function deleteCookie (name) {
+    if (getCookie(name) != null) {
+        setCookie(name, '')
     }
 }
