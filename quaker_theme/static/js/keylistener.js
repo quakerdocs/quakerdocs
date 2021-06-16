94a31185aee97dd5da77fbe7190fee3343c45eb1
Bob Reus <bobreus950@gmail.com>
2021-06-16 11:31:12 +0200

    improve keylistener.js code style

--- quaker_theme/static/js/keylistener.js
+++ quaker_theme/static/js/keylistener.js
@@ -1,36 +1,36 @@
 document.addEventListener('keyup', (event) => {
-    var code = event.code;
+    const code = event.code
 
     /**
      * If user presses Escape and the search bar is open, close the search
      * overlay or bookmark overlay.
      */
-    if (code == 'Escape') {
+    if (code === 'Escape') {
         if (searchOpen) {
-            hideSearchOverlay();
+            hideSearchOverlay()
         }
+
         if (bookmarkOpen) {
-            hideBookmarkOverlay();
+            hideBookmarkOverlay()
         }
     }
 
     // If any overlay is open, do not allow any other overlay to open.
-    var anyOpen = searchOpen || bookmarkOpen;
-
-    if (anyOpen)
-        return;
+    if (searchOpen || bookmarkOpen) {
+        return
+    }
 
     // If the user presses 'S', toggle the search overlay.
-    if (code == 'KeyS') {
+    if (code === 'KeyS') {
         if (!searchOpen) {
-            showSearchOverlay();
+            showSearchOverlay()
         }
     }
 
     // If the user presses 'B', toggle the bookmark overlay.
-    if (code == 'KeyB') {
+    if (code === 'KeyB') {
         if (!bookmarkOpen) {
             showBookmarkOverlay()
         }
     }
-}, false);
+}, false)
